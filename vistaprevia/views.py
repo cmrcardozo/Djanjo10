e59de7c6352d1d44de1d315c37528b9f7f43067b
Carina Cardozo <carinakary@gmail.com>
2018-08-18 16:53:55 -0700

    Se agrego templates

--- vistaprevia/views.py
+++ vistaprevia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hola Mundo!.")
+	contenido = { 'nombre_sitio': 'LibrosOnline' }
+	return render(request, 'vistaprevia/index.html', contenido)
