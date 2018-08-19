e290421ba7ae9ee4e349bcb85c5f0a43f1450163
Carina Cardozo <carinakary@gmail.com>
2018-08-19 12:41:45 -0700

    Se modifico admin.py para cambiar el order de los campos en pantalla

--- vistaprevia/admin.py
+++ vistaprevia/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from vistaprevia.models import Producto
 # Register your models here.
+class ProductoAdmin(admin.ModelAdmin):
+	fields =['fecha_publicacion' ,'producto']
+admin.site.register(Producto, ProductoAdmin)
