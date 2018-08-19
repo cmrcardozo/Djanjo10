008ffd2eda6cf232213e08a70ca837b2b619f60a
Carina Cardozo <carinakary@gmail.com>
2018-08-19 13:12:46 -0700

    Se modifico la tabla producto (ruta imagen yfecha de public)

--- librosonline/settings.py
+++ librosonline/settings.py
@@ -71,7 +71,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'librosonline.wsgi.application'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-MEDIA_ROOT = os.path.join(BASE_DIR, media) 
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, media) 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
