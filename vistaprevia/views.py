810994a0bf946d7880304f5a80019c41e7f77939
Carina Cardozo <carinakary@gmail.com>
2018-08-18 17:29:24 -0700

    Se modifico views.py e index.html para incluir estructuras de decision

--- vistaprevia/views.py
+++ vistaprevia/views.py
@@ -5,4 +5,7 @@ from django.template import RequestContext, loader
 # Create your views here.
 def index(request):
 	contenido = { 'nombre_sitio': 'Mis Libros Online' }
-	return render(request, 'vistaprevia/index.html', contenido)
+	para_minorista = { 'tipo_usuario' : 'minorista', 'incremento' : '25'}
+	para_mayorista = { 'tipo_usuario' : 'mayorista', 'incremento' : '10'}
+	return render(request, 'vistaprevia/index.html', {'contenido':contenido, 'para_minorista':para_minorista, 'para_mayorista':para_mayorista})
+	#return render(request, 'vistaprevia/index.html', contenido)
