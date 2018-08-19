b5e0827b708d519ff5319ed7e1fb8d1a6f9e9fbe
Carina Cardozo <carinakary@gmail.com>
2018-08-19 12:54:09 -0700

    Se agregaron cambios para cargar fotos

--- librosonline/settings.py
+++ librosonline/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'librosonline.wsgi.application'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT = os.path.join(BASE_DIR, media) 
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
